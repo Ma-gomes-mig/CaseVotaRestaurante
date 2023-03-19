95b7674d1683cbf3bb934b83f0ea33a12c7d7c35
Matheus Gomes <matheus.gom23@yahoo.com>
2023-03-19 20:36:51 -0300

    Método get vote

---
+++ CaseVotaRestaurante.WebApi/Controllers/VoteController.cs
@@ -0,0 +1,30 @@
+﻿using CaseVotaRestaurante.Application.DTOs;
+using CaseVotaRestaurante.Application.Interfaces;
+using CaseVotaRestaurante.Application.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaseVotaRestaurante.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VoteController : ControllerBase
+    {
+        private readonly IVoteService _voteService;
+        public VoteController(IVoteService voteService)
+        {
+            _voteService = voteService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<VoteDTO>>> Get()
+        {
+            var vote = await _voteService.GetAllVoteAsync();
+            if (vote == null)
+            {
+                return NotFound("Restaurant not found");
+            }
+            return Ok(vote);
+        }
+    }
+}
